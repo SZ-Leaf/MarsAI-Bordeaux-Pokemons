e811108d232e27712c836edf451965c8aec2e4d8
MathCT <mathieu.chatrain@gmail.com>
2026-01-27 11:04:03 +0100

    first commit front

---
+++ front-end/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello les Pokemons</h1>
+      <Routes>
+          
+      </Routes>
+    </>
+  )
+}
+
+export default App

0342a76ef9bf2a111d409554972f8566546382f0
Victor-Voyer <victor.voyer@oclock.school>
2026-01-28 14:17:36 +0100

    create inputs

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,15 +3,19 @@ import '../App.css';
 
 export default function Homepage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col gap-4">
-      <button className="btn btn-primary">Primary Action</button>
-        <button className="btn btn-secondary">Secondary Action</button>
-        <button className="btn btn-modern">Modern Action</button>
-        <button className="btn btn-danger">Danger Action</button>
-        <button className="btn btn-info">Info Action</button>
-
-      </div>
+    <div>
+      <section className="flex flex-col items-center justify-center h-screen gap-10">
+        <div className="flex flex-col gap-4">
+          <input type="text" className="input-dark" placeholder="Input Dark" />
+          <input type="text" className="input-light" placeholder="Input Light" />
+        </div>
+        <div className="flex flex-col gap-4">
+          <button className="btn btn-primary">Primary Action</button>
+          <button className="btn btn-secondary">Secondary Action</button>
+          <button className="btn btn-modern">Modern Action</button>
+          <button className="btn btn-danger">Danger Action</button>
+        </div>
+      </section>
     </div>
   );
 }

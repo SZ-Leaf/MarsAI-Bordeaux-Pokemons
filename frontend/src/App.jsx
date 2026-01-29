1d85266d39cb218036e3c44977ae84f3defcf2a9
Victor-Voyer <victor.voyer@oclock.school>
2026-01-29 16:15:52 +0100

    create footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import TestsGraphique from './pages/TestsGraphique';
 import Header from './components/layout/header/Header';
+import Footer from './components/layout/footer/Footer';
 
 function App() {
   return (
     <div className="App pt-24" >
       <Header />
       <TestsGraphique />
+      <Footer />
     </div>
   );
 }

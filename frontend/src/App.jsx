d1f35de6375b3ad2b39307e23e02e22850d83812
Victor-Voyer <victor.voyer@oclock.school>
2026-01-29 14:00:13 +0100

    create components header + navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import TestsGraphique from './pages/TestsGraphique';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App pt-24" >
+      <Header />
       <TestsGraphique />
     </div>
   );
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './ui/navbar.css';
+
+const Header = () => {
+  return (
+    <header className="header-pill-container">
+      <div className="header-pill-content">
+        {/* Logo */}
+        <div className="navbar-logo">
+          MARS <span>AI</span>
+        </div>
+
+        {/* Navbar (Icons) */}
+        <Navbar />
+      </div>
+    </header>
+  );
+};
+
+export default Header;

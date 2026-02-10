fc5db9ec41b7ef27cd4b759afaf205a3fd8a66bd
ThibFr33 <thibs.cha@gmail.com>
2026-02-10 09:31:19 +0100

    updating frontend for playlist

--- backend/src/models/selector/selector_memo.model.js
+++ backend/src/models/selector/selector_memo.model.js
@@ -53,6 +53,7 @@ export const createSelectorMemo = async ({ userId, submissionId, rating, comment
     throw new Error(error.message);
   }
 };
+//fonction qui récupère toutes les soumissions d'une playlist pour un utilisateur donné
 
 export async function getPlaylist(user_id, selection_list) {
     const [rows] = await db.pool.execute(
@@ -66,7 +67,7 @@ export async function getPlaylist(user_id, selection_list) {
     );
     return rows;
 };
- 
+//fonction qui ajoute une soumission à une playlist 
 export async function addSubmissionToPlaylist(user_id, submission_id, selection_list) {
     const [result] = await db.pool.execute(`INSERT INTO selector_memo(user_id, submission_id, selection_list) VALUES (?,?,?)
     ON DUPLICATE KEY UPDATE
@@ -75,7 +76,7 @@ export async function addSubmissionToPlaylist(user_id, submission_id, selection_
 
     return result.affectedRows;
 };
-
+//fonction qui retire une soumission d'une playlist et qui la remet à null
 export async function removeSubmissionFromPlaylist(user_id, submission_id) {
     const [result] = await db.pool.execute(`UPDATE selector_memo
       SET selection_list = NULL, updated_at = NOW()
@@ -86,7 +87,7 @@ export async function removeSubmissionFromPlaylist(user_id, submission_id) {
 
     return result.affectedRows;
 };
-
+//récupération du statut d'une playlist pour une soumission donnée et son user_id
 export async function getSelectionForSubmission(user_id, submission_id) {
   const [rows] = await db.pool.execute(
     `SELECT selection_list
@@ -95,5 +96,5 @@ export async function getSelectionForSubmission(user_id, submission_id) {
      LIMIT 1`,
     [user_id, submission_id]
   );
-  return rows[0]?.selection_list ?? null; // "FAVORITES" | "WATCH_LATER" | "REPORT" | null
+  return rows[0]?.selection_list ?? null; // "FAVORITES", "WATCH_LATER", "REPORT", null
 }

f0f4602d92744077d9c5e63e22b0a2528c3f1f66
Sary Zeaiter <saryzeaiter@gmail.com>
2026-02-24 10:02:06 +0100

    add middleware to submissions route

--- backend/src/routes/submission/submission.routes.js
+++ backend/src/routes/submission/submission.routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { submitController, getSubmissionsController, getSubmissionByIdController } from '../../controllers/submissions/submissions.controller.js';
 import { uploadSubmissionFiles, handleUploadError } from '../../middlewares/upload.js';
+import { authenticate, requireRole } from '../../middlewares/auth.middleware.js';
 
 const router = Router();
 
@@ -13,11 +14,15 @@ router.post(
 
 router.get(
   '/',
+  authenticate,
+  requireRole([2, 3]),
   getSubmissionsController
 );
 
 router.get(
   '/:id',
+  authenticate,
+  requireRole([2, 3]),
   getSubmissionByIdController
 );
 
